6c1c7e275daca44320fb9b410cb1a5c69a6e1466
saksham1742683 <sakshamjain1742683@gmail.com>
2025-08-25 17:00:21 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,6 @@ export const useCart = () => {
   return useContext(CartContext);
 };
 
-// --- 2. Create Cart Provider ---
-// This component will wrap our application and provide the cart context to all children.
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
     const [wishListItems, setWishListItems] = useState([]);
@@ -21,20 +19,19 @@ export const CartProvider = ({ children }) => {
   const addToCart = (product) => {
     toast.success('Product added to cart')
     setCartItems((prevItems) => {
-      // Check if the item is already in the cart
+   
       const existingItem = prevItems.find((item) => item.id === product.id);
       if (existingItem) {
-        // If it exists, map over the items and increase the quantity
+
         return prevItems.map((item) =>
           item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
         );
       } else {
-        // If it's a new item, add it to the cart with a quantity of 1
+   
         return [...prevItems, { ...product, quantity: 1 }];
       }
     });
-    console.log("Added to cart:", product.model);
-    // In a real app, you might show a toast notification here
+  
   };
     const addToWishList = (product) => {
          toast.success('Product added to WishList')
@@ -42,7 +39,7 @@ export const CartProvider = ({ children }) => {
       // Check if the item is already in the cart
       const existingItem = prevItems.find((item) => item.id === product.id);
       if (existingItem) {
-        // If it exists, map over the items and increase the quantity
+       
         return prevItems.map((item) =>
           item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
         );
@@ -52,7 +49,7 @@ export const CartProvider = ({ children }) => {
       }
     });
     console.log("Added to cart:", product.model);
-    // In a real app, you might show a toast notification here
+
   };
 
   const value = {
